f2a08f4828b40585f6d1c7217812973c8ae44261
basiltime <krystalcanfield04@gmail.com>
2021-08-11 21:06:19 -0400

    Feature: Started working on user authentic ntication with Bcrypt

--- controllers/users.js
+++ controllers/users.js
@@ -2,10 +2,11 @@ const express = require('express')
 const db = require('../database-connection/db')
 const bcrypt = require('bcryptjs')
 
+
 /* POST Create Account */
 exports.signup = (req, res, next) => {
   bcrypt.hash(req.body.password, 10).then((hash) => {
-    const data = `INSERT INTO users
+    const accountData = `INSERT INTO users
         ( firstName, 
           lastName, 
           email, 
@@ -14,31 +15,30 @@ VALUES ( '${req.body.firstName}',
          '${req.body.lastName}', 
          '${req.body.email}', 
          '${hash}' )`
-    db.query(data, (err, rows) => {
+    db.query(accountData, (err, rows) => {
       if (err) throw err
       console.log('New Account Created')
     })
-    .then(
-        res.status(201).json({
-        message: 'User added successfully!'
-      }))
-  }).catch(
-      (error) => {
-        res.status(500).json({
-            error: error
-            })
-      }
-  )
+   
+  })
 }
 
 
 
+exports.login = (req, res, next) => {
+    const loginData = `SELECT * FROM users WHERE email = '${req.body.email}';`
+    db.query(loginData, (err, results) => {
+    if (err) throw err
+    console.log(req.body.email)
+    console.log(results[0].password)
+  
+    bcrypt.compare(req.body.password, results[0].password, function(err, res) {
+        if (res === true) {console.log('Password Matches!')} 
+        else if (res === false) {console.log('Password does not match!!!')}
+    });
+})  
 
-
-
-
-
-
+}
 
 
 
--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,6 @@ const userCtrl = require('../controllers/users')
 
 
 router.post('/signup', userCtrl.signup);
+router.post('/login', userCtrl.login)
 
 module.exports = router
