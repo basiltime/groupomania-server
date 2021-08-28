5e23989cac024f952f59df8238be95a3f844bd91
basiltime <krystalcanfield04@gmail.com>
2021-08-27 20:38:30 -0400

    Refactor all database calls with prepared statements

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,42 +1,35 @@
 const db = require('../database-connection/db')
 
-exports.newsfeed =  (req, res, next) => {
-    const newsFeed = `SELECT users.userId, users.firstName, users.lastName, posts.textContent, posts.multimediaUrl, posts.timestamp
+exports.newsfeed = (req, res, next) => {
+  const newsFeed = `SELECT users.userId, users.firstName, users.lastName, posts.textContent, posts.multimediaUrl, posts.timestamp
     FROM posts, users
     WHERE users.userId = posts.userId;`
-    db.query(newsFeed, (err, results) => {
-      res.status(200).json({
-      data: results
-      })
+  db.execute(newsFeed, (err, results) => {
+    res.status(200).json({
+      data: results,
+      message: 'Newsfeed Loaded',
     })
-
-console.log('Access to Posts Allowed')
-}
-
-
-
-exports.createPost = (req, res, next) => {
-
-  const postData = `INSERT INTO posts
-  ( 
-    textContent,
-    timestamp,
-    multimediaUrl,
-    userId ) 
-VALUES ( "${req.body.textContent}",
-      "${req.body.timestamp}", 
-      "${req.body.imageUrl}", 
-      "${req.body.userId}")`
-
-db.query(postData, (err) => {
-if (err) { res.status(400).json({message: 'Problem with your post'}), console.log(err) } 
-else {
-console.log('Post Created!')
-  res.status(200).json({
-  message: "you sent back data!"
   })
 }
-})
+
+exports.createPost = (req, res, next) => {
+  db.execute(
+    'INSERT INTO posts ( textContent, timestamp, multimediaUrl, userId ) VALUES (?, ?, ?, ?)',
+    [
+      `${req.body.textContent}`,
+      `${req.body.timestamp}`,
+      `${req.body.imageUrl}`,
+      `${req.body.userId}`,
+    ],
+    (err) => {
+      if (err) {
+        res.status(400).json({ message: 'Problem with your post' }),
+          console.log(err)
+      } else {
+        res.status(200).json({
+          message: 'Post Successful!',
+        })
+      }
+    },
+  )
 }
-
-
--- controllers/users.js
+++ controllers/users.js
@@ -3,100 +3,106 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const dotenv = require('dotenv')
 
-/* POST Create Account */
+/* Create Account */
 exports.signup = (req, res, next) => {
   bcrypt.hash(req.body.password, 10).then((hash) => {
-    const accountData = `INSERT INTO users
-        ( firstName, 
-          lastName, 
-          email, 
-          password ) 
-    VALUES ( '${req.body.firstName}',
-            '${req.body.lastName}', 
-            '${req.body.email}', 
-            '${hash}' )`
-    db.query(accountData, (err, fields) => {
-      if (err && (err.code = 'ER_DUP_ENTRY')) { res.status(409).json({message: 'duplicate email'}) } 
-      else if (err) {res.status(500).json({message: 'something else went wrong'})}
-      else {
-      
-      console.log('New Account Created')
-      let user = fields.insertId
-      console.log(user)
-      const token = jwt.sign(
-        { userId: user },
-        process.env.SECRET_ACCESS_KEY,
-        { expiresIn: '24h' },
-      )
-      res.status(200).json({
-        userId: user,
-        token: token,
-      })
-    }
-    })
+    db.execute(
+      'INSERT INTO users ( firstName, lastName, email, password ) VALUES (?, ?, ?, ?)',
+      [
+        `${req.body.firstName}`,
+        `${req.body.lastName}`,
+        `${req.body.email}`,
+        `${hash}`,
+      ],
+      (err, fields) => {
+        if (err && (err.code = 'ER_DUP_ENTRY')) {
+          res.status(409).json({ message: 'duplicate email' })
+        } else if (err) {
+          res.status(500).json({ message: 'something else went wrong' })
+        } else {
+          console.log('New Account Created')
+          let user = fields.insertId
+          const token = jwt.sign(
+            { userId: user },
+            process.env.SECRET_ACCESS_KEY,
+            { expiresIn: '24h' },
+          )
+          res.status(200).json({
+            userId: user,
+            token: token,
+          })
+        }
+      },
+    )
   })
-
 }
 
-/* POST Login */
+/* Login */
 exports.login = (req, res, next) => {
-  const loginData = `SELECT * FROM users WHERE email = '${req.body.email}';`
-  db.query(loginData, (err, results) => {
-    if (err) throw err
-    
-    if (results[0] === undefined) {
-      res.status(401).json({
-        message: "Invalid email."
-      })
-
-    } else {
-    let user = results[0]
-    bcrypt.compare(req.body.password, user.password, function (err, response) {
-      if (response === true) {
-        console.log('Password Matches!')
-
-        const token = jwt.sign(
-          { userId: user.userId },
-          process.env.SECRET_ACCESS_KEY,
-          { expiresIn: '24h' },
-        )
-
-        res.status(200).json({
-          userId: user.userId,
-          token: token,
-        })
-      } else if (response === false) {
+  db.execute(
+    'SELECT * FROM users WHERE email = ?',
+    [`${req.body.email}`],
+    (err, results) => {
+      if (err) throw err
+      if (results[0] === undefined) {
         res.status(401).json({
-          message: "Incorrect password"
+          message: 'Invalid email.',
+        })
+      } else {
+        let user = results[0]
+        bcrypt.compare(req.body.password, user.password, function (
+          err,
+          response,
+        ) {
+          if (response === true) {
+            console.log('Password Matches!')
+            const token = jwt.sign(
+              { userId: user.userId },
+              process.env.SECRET_ACCESS_KEY,
+              { expiresIn: '24h' },
+            )
+            res.status(200).json({
+              userId: user.userId,
+              token: token,
+            })
+          } else if (response === false) {
+            res.status(401).json({
+              message: 'Incorrect password',
+            })
+          }
         })
       }
-    })
-  }
-  })
+    },
+  )
 }
 
+/* View Account */
 exports.viewAccount = (req, res, next) => {
-  const userData = `SELECT * FROM users WHERE userId = '${req.params.id}'`;
-
-  db.query(userData, (err, results) => {
-    let user = results[0]
-    if (err) throw err
-    res.status(200).json({
-      firstName: `${user.firstName}`,
-      lastName: `${user.lastName}`,
-      email: `${user.email}`
-    })
-  })
- 
+  db.execute(
+    'SELECT * FROM users WHERE userId = ?',
+    [`${req.params.id}`],
+    (err, results) => {
+      let user = results[0]
+      if (err) throw err
+      res.status(200).json({
+        firstName: `${user.firstName}`,
+        lastName: `${user.lastName}`,
+        email: `${user.email}`,
+      })
+    },
+  )
 }
 
+/* Delete Account */
 exports.deleteAccount = (req, res, next) => {
-  const deleteUser = `DELETE FROM users WHERE userId = '${req.params.id}'`;
-
-  db.query(deleteUser, (err, results) => {
-    if (err) throw err
-    res.status(200).json({
-      message: 'Account successfully deleted'
-    })
-  })
+  db.execute(
+    'DELETE FROM users WHERE userId = ?',
+    [`${req.params.id}`],
+    (err) => {
+      if (err) throw err
+      res.status(200).json({
+        message: 'Account successfully deleted',
+      })
+    },
+  )
 }
