7b987f7196ee8401a947971cb3584c853f52060d
basiltime <krystalcanfield04@gmail.com>
2021-09-14 18:50:17 -0400

    Attempt connection to Heroku
    jfkdasl
    
    kfdsjakl
    
    jdfsaklfjds
    
    jfdakl
    
    jfkdaj
    
    fjkd
    
    jfkdsal
    
    fjdsk
    
    jfkdls
    
    jfklds
    
    jsfkasd
    
    jfkdsl
    
    jfkdasl
    
    jfkdlsa
    
    jfksa
    
    jfkdsl
    
    jfkds
    
    jkflds

--- database-connection/db.js
+++ database-connection/db.js
@@ -3,11 +3,12 @@ const mysql = require('mysql2')
 
 
 // Create Connection
-const db = mysql.createConnection({
+const db = mysql.createPool({
+  connectionLimit: 4,
   host      : 'us-cdbr-east-04.cleardb.com',
   user      : 'b1e90c8bcbdf6f',
   password  : 'ba142b90',
-  database  : 'heroku_d41279214193bc7',
+  database  : 'heroku_d41279214193bc7'
   })
 
 module.exports = db;
