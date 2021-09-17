c454d11971109646b74d4cecb055df7ccc2828d9
basiltime <krystalcanfield04@gmail.com>
2021-09-16 23:14:19 -0400

    undo

--- controllers/likes.js
+++ controllers/likes.js
@@ -11,8 +11,9 @@ exports.like = (req, res, next) => {
       if (results.length == 0) {
         db.execute(
           'INSERT INTO likes ( postId, userId ) VALUES ( ?, ?)',
-          [`${req.body.postId}`, `${req.body.userId}`], 'SELECT * FROM likes',
-          (err, results) => {
+          [`${req.body.postId}`, `${req.body.userId}`],(err, results) => {},
+        )
+        db.execute(`SELECT * FROM likes`, (err, results) => {
           res.status(200).json({
               data: results
           })
@@ -39,40 +40,3 @@ exports.getAllLikes = (req, res, next) => {
       })
   })
 }
-
-
-
-
-// exports.like = (req, res, next) => {
-//   // Request body contains userId and postId. If the user hasn't already liked the post, 
-//   // their like is inserted into the database. Else, their like is removed.
-//   db.execute(
-//     'SELECT * FROM likes WHERE postId = ? AND userId = ?',
-//     [`${req.body.postId}`, `${req.body.userId}`],
-//     (err, results) => {
-//       if (results.length == 0) {
-//         db.execute(
-//           'INSERT INTO likes ( postId, userId ) VALUES ( ?, ?)',
-//           [`${req.body.postId}`, `${req.body.userId}`],(err, results) => {
-
-//           },
-//         )
-//         db.execute(`SELECT * FROM likes`, (err, results) => {
-//           res.status(200).json({
-//               data: results
-//           })
-//       })
-//       } else if (!results.length == 0) {
-//         db.execute(
-//           'DELETE FROM likes WHERE ( postId, userId ) = ( ?, ? );', 
-//           [`${req.body.postId}`, `${req.body.userId}`], (err, results) => {},
-//         )
-//         db.execute(`SELECT * FROM likes`, (err, results) => {
-//           res.status(200).json({
-//               data: results
-//           })
-//       })
-//       }
-//     },
-//   )
-// }
