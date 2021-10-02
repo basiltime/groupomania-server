997cd952cae4e6446f33d8e5eb8c9f885c76522c
basiltime <krystalcanfield04@gmail.com>
2021-10-02 12:20:13 -0400

    Remove cors package

--- app.js
+++ app.js
@@ -7,19 +7,18 @@ const commentsRouter = require('./routes/comments')
 const likesRouter = require('./routes/likes')
 const homeRouter = require('./routes/home')
 const app = express()
-const cors = require('cors')
+
 
 
 app.use(express.json())
-app.use(cors())
 app.use(express.urlencoded({ extended: false }))
 
-// app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-//   next();
-// });
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
 
 
 app.use('/posts', postsRouter)
